6fa8c88e432a72998144414050ddbeab60534044
TingYu <b860627@gmail.com>
2022-03-01 18:49:19 +0800

    correct incorrect webservlet

--- src/main/java/com/members/controller/MembersLoginServlet.java
+++ src/main/java/com/members/controller/MembersLoginServlet.java
@@ -13,7 +13,7 @@ import com.members.model.MembersDAO;
 import com.members.model.MembersDAO_interface;
 import com.members.model.MembersVO;
 
-@WebServlet("/members/MembersDataUpdate")
+@WebServlet("/members/MembersLogin")
 public class MembersLoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
