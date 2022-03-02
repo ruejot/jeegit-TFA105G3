e69e350c1951d24d8bf6cae87811d778a51ca991
Szu-An <im_lu3@hotmail.com>
2022-03-02 14:07:02 +0800

    fiz  BusLogoutServlet: 路徑寫錯

--- src/main/java/com/bus/controller/BusLogoutServlet.java
+++ src/main/java/com/bus/controller/BusLogoutServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("bus/BusLogout")
+@WebServlet("/bus/BusLogout")
 public class BusLogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
